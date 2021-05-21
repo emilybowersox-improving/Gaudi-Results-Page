7bf3f933b25238f2bdf6abb87032cddd48efbf97
emilybowersox <emilygbowersox@gmail.com>
2021-05-21 16:03:36 -0500

    next task is creating the hidden div

--- gaudi.js
+++ gaudi.js
@@ -2,15 +2,31 @@ const imageArray = ["imgs/1.jpg", "imgs/2.jpg", "imgs/3.jpg", "imgs/4.jpg", "img
 
 const images = document.querySelector('#images');
 
-
-
-
-for (i=0; i < imageArray.length; i++) {
+for (i=0; i < 100; i++) {
+    const random = Math.floor(Math.random() * imageArray.length); 
     const newImage = document.createElement('img');
-    newImage.src = imageArray[i]; 
+    newImage.src = imageArray[random]; 
     images.appendChild(newImage);
+    console.log(random);
 }   
 
 
 
 
+// //hiding container 
+//https://css-tricks.com/comparing-various-ways-to-hide-things-in-css/
+// ^ Method 1 
+
+// const button = document.querySelectorAll("button");
+// button.forEach((button) =>
+//   button.addEventListener("click", () => alert("You clicked a button"))
+// );
+
+// // Demo only
+
+// const checkbox = document.getElementById("toggle");
+const hiddenDiv = document.getElementById("hide");
+
+// checkbox.addEventListener("change", (e) => {
+//   hiddenButton.classList.toggle("hidden");
+// });
